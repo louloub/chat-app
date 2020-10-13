fde73a2c6c129719658d06eef0967045e446bb0b
loulou <yourdj.france@gmail.com>
2020-10-13 11:45:38 +0200

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,27 @@
 import React from 'react';
+import './myCSS.css'
 
 function Contact(){
     return (
        <div>
-           <div>
-               <img src={contact1.image}></img>
-               <h1>{contact1.firstName}</h1>
-               <h3>{contact1.lastName}</h3>
-           </div>
-           <div>
-               <img src={contact2.image}></img>
-               <h1>{contact2.firstName}</h1>
-               <h3>{contact2.lastName}</h3>
-           </div>
-           <div>
-               <img src={contact3.image}></img>
-               <h1>{contact3.firstName}</h1>
-               <h3>{contact3.lastName}</h3>
+           <div class="Contact">
+               <img class="avatar" src={contact1.avatar}></img>
+               <div>
+                    <h1 class="name">{contact1.name}</h1> 
+                    <div class="status">
+                        <img src="" class="status-online"></img>
+                        <h2 class="status-text">{contact1.status}</h2> 
+                    </div>
+               </div>
            </div>
        </div> 
     )
 }
 
 const contact1 = {
-    image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
-    firstName: "Bart 1",
-    lastName: "Simpson 1"
-  };
-
-const contact2 = {
-    image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
-    firstName: "Bart 2",
-    lastName: "Simpson 2"
-};
-
-const contact3 = {
-    image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
-    firstName: "Bart 3",
-    lastName: "Simpson 3"
+    avatar: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+    name: "Bart 1",
+    status: "online"
   };
 
 export default Contact;
