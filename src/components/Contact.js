b1dacdd8b19e2a33ecd7e16e201aebe0238015ba
loulou <yourdj.france@gmail.com>
2020-10-14 15:38:53 +0200

    final React 05 - State challenge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import './myCSS.css'
 
-function Contact(props){
-    return (
-        <div class="Contact">
-            <img class="avatar" alt="" src={props.avatar} />
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online,
+        }
+    }
+
+    render() {
+      return (
+        <div className="Contact">
+            <img className="avatar" alt="" src={this.props.avatar} />
             <div>
-                <h3 class="name">{props.name}</h3>
-                <div class="status">
-                    <div class={props.online ? "status-online" : "status-offline"} />
-                    <p class="status-text">{props.online ? "online" : "offline"}</p>  
+                <h3 className="name">{this.props.name}</h3>
+                <div className="status" 
+                    onClick={ event => {
+                        this.state.online 
+                            ? this.setState({ online : false}) 
+                            : this.setState({ online : true})
+                            console.log("click ok")
+                        }
+                    }>
+
+                    <div 
+                        className={this.state.online 
+                            ? "status-online" 
+                            : "status-offline"} 
+                    />
+                    <p className="status-text">{this.state.online 
+                        ? "online" 
+                        : "offline"}</p>  
                 </div>
             </div>
         </div>
-    )
-}
+      );
+    }
+  }
 
 export default Contact;
